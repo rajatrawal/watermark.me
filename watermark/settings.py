5c0a9e4e4130642f2fd27301a4665bdf510e9f8c
Rajat <rwlrajat@gmail.com>
2023-04-21 15:23:02 +0530

    initail commit

--- watermark/settings.py
+++ watermark/settings.py
@@ -78,10 +78,14 @@ WSGI_APPLICATION = 'watermark.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'postgres',
+       'USER': 'postgres',
+       'PASSWORD': 'ZLIGzpek9Amc5Wiu',
+       'HOST': 'db.sspjyzwsetbtfrlqogvg.supabase.co',
+       'PORT': '5432',
+   }
 }
 
 
@@ -147,3 +151,4 @@ MESSAGE_TAGS = {
     messages.WARNING: 'alert-warning',
     messages.ERROR: 'alert-danger',
 }
+
