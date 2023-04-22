37bc7778aad89717841d1eaeb0eded04b157e2d4
Rajat <rwlrajat@gmail.com>
2023-04-22 13:07:24 +0530

    h commit

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,5 @@
 echo " BUILD START"
 python3.9 -m pip install -r requirements.txt
-chgrp -R www-data /var/task/media/images/
-chgrp -R www-data /var/task/media/images/
-chmod -R g+w  /var/task/media/temp/
-chmod -R g+w  /var/task/media/temp/
+chmod -R g+w  /media/temp/
+chmod -R g+w  /media/temp/
 echo " BUILD END" 
