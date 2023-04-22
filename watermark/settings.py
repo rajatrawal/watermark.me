de4c358852441e84c69836404b12b9ee1df11ea2
Rajat <rwlrajat@gmail.com>
2023-04-22 13:45:46 +0530

    initial commit

--- watermark/settings.py
+++ watermark/settings.py
@@ -130,11 +130,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR , 'static')
 
 
 
